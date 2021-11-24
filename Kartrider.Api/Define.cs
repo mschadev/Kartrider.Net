72530f0460a471e5685b47672396ac24f7a92d1a
Plorence <ploseok@gmail.com>
2021-11-24 22:17:55 +0900

    REQUEST_DATE_TIME_FORMAT 상수 정의

---
+++ Kartrider.Api/Define.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kartrider.Api
+{
+    internal static class Define
+    {
+       public const string REQUEST_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    }
+}
