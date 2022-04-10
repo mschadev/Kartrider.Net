5794b9782ccb237308cfbe5b69fc63a5c3145e45
Plorence <ploseok@gmail.com>
2022-04-10 13:47:38 +0900

    test: Add Json_Read

--- Kartrider.Api.Test/Jsons/MatchDetailTests.cs
+++ Kartrider.Api.Test/Jsons/MatchDetailTests.cs
@@ -1,4 +1,5 @@
-﻿using Kartrider.Api.Endpoints.MatchEndpoint.Models;
+﻿using System;
+using Kartrider.Api.Endpoints.MatchEndpoint.Models;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -27,6 +28,34 @@ namespace Kartrider.Api.Test.Jsons
                 });
             }
         }
-        // Json Read는 메서드 내부적으로 진행하므로 스킵
+
+        [DataTestMethod]
+        [DataRow("02480011c20941a3")]
+        [DataRow("02480011c213bed1")]
+        [DataRow("02480011c216268a")]
+        [DataRow("02480011c21b59b7")]
+        [DataRow("02480011c21d9fee")]
+        [DataRow("02480011c21fd637")]
+        [DataRow("02480011c223049b")]
+        [DataRow("02480011c2256688")]
+        [DataRow("02480011c2279c15")]
+        [DataRow("02480011c22a9a80")]
+        [DataRow("02480011c22cccf1")]
+        [DataRow("02480011c22f52c1")]
+        [DataRow("02480011c2317080")]
+        [DataRow("02480011c2345374")]
+        [DataRow("02480011c2370c99")]
+        [DataRow("032b000dc4945b28")]
+        public async Task Json_Read(string matchId)
+        {
+            try
+            {
+                await kartriderApi.Match.GetMatchDetailAsync(matchId);
+            }
+            catch (JsonException e)
+            {
+                Assert.Fail(e.ToString());
+            }
+        }
     }
 }
